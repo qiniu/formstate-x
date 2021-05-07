c6115fd4bf0da1355778564ace56f3a2f509bc54
nighca <nighca@live.cn>
2021-05-07 15:00:57 +0800

    remove useless line

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 export * from './types'
-
 export { default as FieldState } from './fieldState'
 export * from './fieldState'
-
-export { FormState, ArrayFormState } from './formState'
 export * from './formState'
-
 export * from './bind'
