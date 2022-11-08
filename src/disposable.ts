1182849417e80097b392346c01fdfca9d7e5bd6e
lzfee0227 <lzfee0227@gmail.com>
2022-11-08 10:56:00 +0800

    make addDisposer public

--- src/disposable.ts
+++ src/disposable.ts
@@ -13,7 +13,7 @@ export default class Disposable {
   /**
    * Collect the disposer function.
    */
-  protected addDisposer(disposer: Disposer) {
+  addDisposer(disposer: Disposer) {
     this.disposers.push(disposer)
   }
 
